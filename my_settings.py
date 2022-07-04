3de53680cfb2f03622a20580526a38ec5ea97d09
WorkYong <kyong@WorkYongui-MacBookAir.local>
2022-07-04 13:27:44 +0900

    Add: tuto Django Settings

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tuto',
+        'USER': 'root',
+        'PASSWORD': 'Qwer1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+
+SECRET_KEY = 'django-insecure-hoz^x9pbtx4x))b980+php)r=0k*ra5uz618hwj7f^(^f_kjeo'
